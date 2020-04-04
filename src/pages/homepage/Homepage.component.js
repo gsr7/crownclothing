18f7201bc5ec3d38ca0e49daba56827ccee8a2fb
Gowri <gowris7@hotmail.com>
2020-04-04 14:23:39 +1100

    broken our components and added styling

---
+++ src/pages/homepage/Homepage.component.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Homepage.styles.scss';
+import Directory from '../../components/directory/Directory.component';
+
+
+
+
+
+
+const HomePage = () => (
+
+
+<div className='homepage'>
+	<Directory />
+	
+</div>
+
+
+);
+
+export default HomePage
