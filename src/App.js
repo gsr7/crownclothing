f75693ede159b65ca96a3c213595d2625a8029d3
Gowri <gowris7@hotmail.com>
2020-04-08 16:20:06 +1000

    added firebase to app and created Google Sign In button in SignIn component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import HomePage from './pages/homepage/Homepage.component';
 import './App.css';
 import {Route,Switch} from 'react-router-dom';
 import ShopPage  from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
 const HatsPage=()=>(
 		<div>
 			<h1> HATS PAGE </h1>
@@ -15,9 +18,12 @@ const HatsPage=()=>(
 function App() {
   return (
     <div>
+      <Header />
     	<Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUp} />
+
         </Switch>
     </div>
   );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+const config=  {
+    apiKey: "AIzaSyDwJaTUCxnH84N0_u2ZN9uQwgVpNiEVbJ0",
+    authDomain: "crwn-db-89b72.firebaseapp.com",
+    databaseURL: "https://crwn-db-89b72.firebaseio.com",
+    projectId: "crwn-db-89b72",
+    storageBucket: "crwn-db-89b72.appspot.com",
+    messagingSenderId: "389768188150",
+    appId: "1:389768188150:web:fe3177dc9c6cfcfdf51cd7",
+    measurementId: "G-HEG1DFD6BV"
+  };
+
+  firebase.initializeApp(config);
+
+  export const auth= firebase.auth();
+  export const firestore = firebase.firestore();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({prompt:'select_account'});
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+  export default firebase;
